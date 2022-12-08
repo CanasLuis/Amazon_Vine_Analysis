2a780235fbf726474f6c91781017a8f09344c823
CanasLuis <108499271+CanasLuis@users.noreply.github.com>
2022-12-08 10:32:26 -0600

    Add files via upload

---
+++ challenge_schema_starter.sql
@@ -0,0 +1,26 @@
+CREATE TABLE enero21 (
+  yearr INTEGER,
+  Monthh INTEGER,
+  DayofMonth INTEGER,
+  DayofWeek INTEGER,
+  Operating_Airline TEXT,
+  Origing TEXT,
+  Dest TEXT,
+  CRSDepTime INTEGER,
+  DepTime INTEGER,
+  DepDelayMinutes INTEGER,
+  CRSArrTime INTEGER,
+  ArrTime INTEGER,
+  Cancelled BOOLEAN,
+  CancellationCode TEXT,
+  CRSElapsedTime INTEGER,
+  ActualElapsedTime INTEGER,
+  CarrierDelay INTEGER,
+  WeatherDelay INTEGER,
+  NASDelay INTEGER,
+  SecurityDelay INTEGER,
+  LateAircraftDelay INTEGER
+);
+
+
+Select * from enero21
